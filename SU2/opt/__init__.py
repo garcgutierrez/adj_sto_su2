22ec37839ed0a08f5dbe1935d18205f085b28a70
garcgutierrez <garcgutierrez@gmail.com>
2020-07-14 17:38:22 +0200

    Codigo py an tutorial
    Estado inicial

---
+++ SU2/opt/__init__.py
@@ -0,0 +1,10 @@
+# SU2/opt/__init__.py
+
+from .project import Project
+from .project_stochastic import Project_Stochastic
+from .scipy_tools import scipy_slsqp as SLSQP
+from .scipy_tools import scipy_cg as CG
+from .scipy_tools import scipy_bfgs as BFGS
+from .scipy_tools import scipy_powell as POWELL
+
+from .UQ import UQ
